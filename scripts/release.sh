30b1dca7d80a79796c637678b0728f3a1f072dfb
andersonbosa <andersonbosa0@gmail.com>
2023-03-24 08:28:57 -0300

    Create Git tag and build release for version specified in package.json

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,20 +1,88 @@
 #!/bin/bash
 
+# CONSTANTS
 VERSION=$(jq -r '.version' package.json)
 TAGS=$(git tag --list)
 
-if [[ "$TAGS" =~ "$VERSION" ]]; then
-  echo "Error: a tag for version $VERSION already exists"
-  exit 1
-else
-  echo "No tag for version $VERSION found"
-  read -p "Do you want to create a Git tag for this version? (y/n): " CHOICE
-  if [[ "$CHOICE" =~ ^[Yy]$ ]]; then
-    git tag -a "$VERSION" -m "Tagging version $VERSION"
-    echo "Git tag $VERSION created successfully"
-  else
-    echo "No Git tag created"
+# EXIT CODES
+EXIT_SUCCESS=0
+EXIT_BY_ANY=1
+EXIT_BY_USER=2
+
+# CAN BE USER ARG
+IS_VERBOSE=false
+
+#!/bin/bash
+
+function log() {
+  local type="${1:-info}"
+  shift
+  local message="$@"
+  local timestamp=""
+  local color=""
+
+  case "$type" in
+  "SUCCESS") color="\033[0;32m" ;;
+  "INFO") color="\033[0;34m" ;;
+  "WARN") color="\033[0;33m" ;;
+  "ERROR") color="\033[0;31m" ;;
+  *) color="\033[0;34m" ;;
+  esac
+
+  if [ "$IS_VERBOSE" = true ]; then
+    timestamp="$(date +"[%Y-%m-%d %H:%M:%S]") "
   fi
+
+  echo -e "$timestamp[$color${type^^}\033[0m] $message" >&2
+}
+
+function tag_version() {
+  git tag -a "$VERSION" -m "Tagging version $VERSION"
+  if [ $? -eq $EXIT_SUCCESS ]; then
+    log "success" "Git tag $VERSION created successfully"
+  else
+    log "error" "Git tag $VERSION it was not created!"
+  fi
+}
+
+function build_release() {
+  log "info" "Building release to $VERSION"
+
+  npm run install
+
+  npm run build
+
+  mv -v ./build ./release
+}
+
+function push_version() {
+  git push origin "$VERSION"
+  if [ $? -eq $EXIT_SUCCESS ]; then
+    log "success" "Git tag $VERSION pushed to remote repository successfully"
+  else
+    log "error" "Failed to push Git tag $VERSION to remote repository"
+  fi
+}
+if [[ "$TAGS" =~ "$VERSION" ]]; then
+  log "error" "Error: a tag for version $VERSION already exists"
+  exit $EXIT_BY_ANY
 fi
 
-# The rest of your script goes here
+log "info" "No tag for version $VERSION found. Continuing ..."
+
+read -p "[INPUT] Do you want to create a Git tag to release this version? (y/n): " CHOICE
+if [[ "$CHOICE" =~ ^[Yy]$ ]]; then
+  tag_version
+else
+  log "INFO" "No Git tag created and exiting..."
+  exit $EXIT_BY_USER
+fi
+
+build_release
+push_version
+if [ $? -eq $EXIT_SUCCESS ]; then
+  log "success" 'Access the link to setup a Release on Github'
+  echo -n '\t > "https://github.com/andersonbosa/automocker.js/releases/new"'
+else
+  log "error" "Something was wrong ...For now I can't help you 🥲"
+fi
